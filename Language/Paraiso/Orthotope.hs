79b35dfb1859657a09ce7a9bf13d021e70655cd2
Takayuki Muranushi <muranushi@gmail.com>
2011-04-10 23:53:21 +0900

    rename data Z -> data Orthotope0

--- Language/Paraiso/Orthotope.hs
+++ Language/Paraiso/Orthotope.hs
@@ -1,22 +1,21 @@
 {-# LANGUAGE TypeOperators #-}
 
 module Language.Paraiso.Orthotope(
-  Z(..),
-  Orthotope0,Orthotope1,Orthotope2,Orthotope3
+  Orthotope0(..),
+  Orthotope1,Orthotope2,Orthotope3
 ) where
 
 import Language.Paraiso.Set as Set
 import Language.Paraiso.Interval
 import Data.Array.Repa ((:.)(..))
 
-data Z a = Z0 | Z deriving (Eq,Ord,Show,Read)
+data Orthotope0 a = Z0 | Z deriving (Eq,Ord,Show,Read)
 
-type Orthotope0 a = Z a
 type Orthotope1 a = Orthotope0 a :. Interval a
 type Orthotope2 a = Orthotope1 a :. Interval a
 type Orthotope3 a = Orthotope2 a :. Interval a
 
-instance Set (Z a) where
+instance Set (Orthotope0 a) where
   empty = Z0
   null Z0 = True
   null Z = False
