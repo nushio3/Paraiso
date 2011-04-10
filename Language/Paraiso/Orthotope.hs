4b81a5044184b6e74bfa961564ca1bbeb2d3c327
Takayuki Muranushi <muranushi@gmail.com>
2011-04-10 23:36:03 +0900

    orthotope jujitsu

--- Language/Paraiso/Orthotope.hs
+++ Language/Paraiso/Orthotope.hs
@@ -1,4 +1,30 @@
-import Language.Paraiso.Interval
-import Data.Array.Repa ((:.))
+{-# LANGUAGE TypeOperators #-}
 
-data Z = Z
+module Language.Paraiso.Orthotope(
+  Z(..),
+  Orthotope0,Orthotope1,Orthotope2,Orthotope3
+) where
+
+import Language.Paraiso.Set as Set
+import Language.Paraiso.Interval
+import Data.Array.Repa ((:.)(..))
+
+data Z a = Z deriving (Eq,Ord,Show,Read)
+
+type Orthotope0 a = Z a
+type Orthotope1 a = Orthotope0 a :. Interval a
+type Orthotope2 a = Orthotope1 a :. Interval a
+type Orthotope3 a = Orthotope2 a :. Interval a
+
+instance Set (Z a) where
+  empty = error "Zero dimensional Orthotope can never be empty"
+  null _ = False
+  intersection _ _ = Z
+  
+instance (Set a, Set b) => Set (a :. b) where
+  empty = empty :. empty
+  null (a:.b) = Set.null a || Set.null b
+  intersection (a1:.b1) (a2:.b2) = 
+    let ret = intersection a1 a2 :. intersection b1 b2 in
+    if Set.null ret then empty else ret
+                                    
--- Language/Paraiso/Set.hs
+++ Language/Paraiso/Set.hs
@@ -1,5 +1,6 @@
 module Language.Paraiso.Set (Set(..)) where
 class Set a where
   empty :: a
+  null :: a -> Bool
   intersection :: a -> a -> a
   
