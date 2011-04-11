61cbe48d788a4e7851b64a471a16a57c3c497554
Takayuki Muranushi <muranushi@gmail.com>
2011-04-11 09:42:47 +0900

    Set theory added

--- Language/Paraiso/Set.hs
+++ Language/Paraiso/Set.hs
@@ -1,4 +1,14 @@
 module Language.Paraiso.Set (Set(..)) where
+{-
+In mathematics, a π-system on a set Ω is a set P, consisting of certain subsets of Ω, such that
+
+    * P is non-empty.
+
+    * A ∩ B ∈ P whenever A and B are in P.
+
+That is, P is a non-empty family of subsets of Ω that is closed under finite intersections.
+-}
+
 class Set a where
   empty :: a
   null :: a -> Bool
