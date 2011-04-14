a68334d5f0515dc19347f1fc11174ea66b80797a
Takayuki Muranushi <muranushi@gmail.com>
2011-04-14 22:32:03 +0900

    getComponent passes type check, now implementing the ingredients

--- Language/Paraiso/Axis.hs
+++ Language/Paraiso/Axis.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE TypeOperators, MultiParamTypeClasses, FlexibleInstances #-}
 module Language.Paraiso.Axis
     (
-     (:.)(..), Vec(..), Vector(..)
+     (:.)(..), Axis(..),
+     Vec(..), Vector(..),
+     Vec1, Vec2, Vec3
     ) where
 
 import Data.Array.Repa ((:.)(..))
@@ -10,8 +12,15 @@ data Axis = Axis Int deriving (Eq,Ord,Show,Read)
 
 data Vec = Vec deriving (Eq,Ord,Show,Read)
 
+type Vec1 a = Vec :. a
+type Vec2 a = Vec1 a :. a
+type Vec3 a = Vec2 a :. a
+
 class Vector v a where
   getComponent :: v a -> Axis -> a
 
 instance Vector ((:.) Vec ) a where
-  getComponent (Vec :. x) _ = x
+  getComponent (Vec :. x) axis = x
+  
+instance (Vector v a) => Vector ((:.)(v a)) a where
+  getComponent (_ :. x) axis = x
--- attic/TestAxis.hs
+++ attic/TestAxis.hs
@@ -2,10 +2,11 @@
 
 import Language.Paraiso.Axis
 
-x :: Vec2 Int
-x = Vec :. 4 :. 2
+x :: Vec3 Int
+x = Vec :. 3 :. 9 :. 8
 
 main :: IO ()
 main = do
   putStrLn $ "x = " ++ show x
+  putStrLn $ "x = " ++ show (getComponent x (Axis 0))
   
