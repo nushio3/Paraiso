d472ec98c79c2cb9323a75e0d8f118b9de99059b
Takayuki Muranushi <muranushi@gmail.com>
2011-04-14 19:28:15 +0900

    PiSystem combination instanciation rule moved to PiSystem

--- Language/Paraiso/Orthotope.hs
+++ Language/Paraiso/Orthotope.hs
@@ -26,10 +26,3 @@ instance PiSystem (Orthotope0 a) where
   intersection Z Z = Z
   intersection _ _ = Z0
   
-instance (PiSystem a, PiSystem b) => PiSystem (a :. b) where
-  empty = empty :. empty
-  null (a:.b) = S.null a || S.null b
-  intersection (a1:.b1) (a2:.b2) = 
-    let ret = intersection a1 a2 :. intersection b1 b2 in
-    if S.null ret then empty else ret
-                                    
--- Language/Paraiso/PiSystem.hs
+++ Language/Paraiso/PiSystem.hs
@@ -1,9 +1,21 @@
+{-# LANGUAGE TypeOperators #-}
+
 module Language.Paraiso.PiSystem (PiSystem(..)) where
 {- In mathematics, a pi-system is a non-empty family of sets that is closed
 under finite intersections.  -}
+
+import Prelude hiding (null)
+import Language.Paraiso.Axis
 
 class PiSystem a where
   empty :: a
   null :: a -> Bool
   intersection :: a -> a -> a
   
+instance (PiSystem a, PiSystem b) => PiSystem (a :. b) where
+  empty = empty :. empty
+  null (a:.b) = null a || null b
+  intersection (a1:.b1) (a2:.b2) = 
+    let ret = intersection a1 a2 :. intersection b1 b2 in
+    if null ret then empty else ret
+                                    
