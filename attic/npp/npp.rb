22c6cff9d37da239397590f4d5cf17816f4129b4
Takayuki Muranushi <muranushi@gmail.com>
2011-04-16 08:01:18 +0900

    array support for npp

--- attic/npp/npp.rb
+++ attic/npp/npp.rb
@@ -1,15 +1,21 @@
 #!/usr/bin/env ruby
 
 TmpFn = "Tmp.hs"
+MagicOpen  = "mMaGiCoPeN"
+MagicClose = "mMaGiClOsE"
 
 def gen(str)
-  words = str.gsub(/\W/,' ').split(/\s+/)
+  str2 = str.gsub('[',MagicOpen).gsub(']',MagicClose)
+  words = str2.gsub(/[^\w\[\]]/,' ').split(/\s+/)
+    
   vars = []
   words.each{|word|
     vars << word if word =~ /^[A-Za-z]/
   }
   vars = vars.uniq
   
+  
+
   genvars = "[#{vars.join(',')}] = map Term $ words \"#{vars.join(' ')}\""
   
   open(TmpFn,'w') {|fp|
@@ -17,7 +23,7 @@ def gen(str)
 import NPP
 
 input :: Expr
-input = #{str}
+input = #{str2}
     where  
       #{genvars}
 
@@ -26,7 +32,7 @@ main = do
   putStrLn $ gen SSE2v2r8 $ optimize input
 HASKELL
   }
-  `runhaskell #{TmpFn}`
+  `runhaskell #{TmpFn}`.gsub(MagicOpen,'[').gsub(MagicClose,']')
 end
 
 while line = gets
