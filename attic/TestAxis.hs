c72cf08c5d25d0f0d586032678165d7083d12f81
nushio <muranushi@gmail.com>
2011-04-18 20:03:26 +0900

    tensor test

--- attic/TestAxis.hs
+++ attic/TestAxis.hs
@@ -12,6 +12,9 @@ x = Vec :. 3 :. 9 :. 8
 z :: Vec3 Double
 z = zeroVector
 
+ten :: Vec2 (Vec2 Int)
+ten = Vec :. (Vec :. 1 :. 0) :. (Vec :. 0 :. 1)
+
 z6 :: Vec6 Double
 z6 = zeroVector
 
@@ -21,7 +24,8 @@ main = do
   putStrLn $ "zeroVector = " ++ show z
   putStrLn $ "zeroVector = " ++ show z6
   forM_ (map Axis [0..dimension x - 1]) sub
-  
+  putStrLn $ "a tensor = " ++ show ten
+        
 sub :: Axis -> IO ()
 sub ex = do
   putStrLn $ "x[" ++ show ex ++ "] = " ++ show (getComponent ex x)
