8f5cfd5bd665c6e1609b08f327818b1466480371
Takayuki Muranushi <muranushi@gmail.com>
2011-04-27 16:38:33 +0900

    graph branch created.

--- Language/Paraiso/OM/Graph.hs
+++ Language/Paraiso/OM/Graph.hs
@@ -1,3 +1,32 @@
 {-# OPTIONS -Wall #-}
 
 import qualified Data.Graph.Inductive as G
+
+
+data NodeL = NodeL {inst::Inst, anot::Annotation}
+data EdgeL = EdgeL
+
+type Gr = G.Gr NodeL EdgeL
+
+data Operator = 
+    Add |
+    Mul | 
+    Sub |
+    Div |
+    Inv |
+    Neg |
+    Madd |
+    Msub |
+    Nmadd |
+    Nmsub 
+
+
+
+arity :: Operator -> (Int, Int) 
+
+
+
+arityI, arityO :: Operator -> Int
+arityI = fst.arity
+arityO = snd.arity
+  
