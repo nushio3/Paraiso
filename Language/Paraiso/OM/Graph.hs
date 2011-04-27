019eb20b7941e0863ec38a2553ad95e3873e4455
Takayuki Muranushi <muranushi@gmail.com>
2011-04-27 23:21:20 +0900

    toriaezu graph

--- Language/Paraiso/OM/Graph.hs
+++ Language/Paraiso/OM/Graph.hs
@@ -1,25 +1,49 @@
 {-# OPTIONS -Wall #-}
 module Language.Paraiso.OM.Graph() where
 
-
+import Language.Paraiso.Interval
 import qualified Data.Graph.Inductive as G
 
 
-data NLabel v = NOperand {operandID::Int, anots::[Annotation]} |
-               NOperator {inst::Inst v, anots::[Annotation]}
-data ELabel v = ELabel
+data NLabel v s = Operand {opID::Int, extent::v (Interval s), homogeneous::Bool, 
+                           anots::[Annotation]} |
+                  Operator {inst::Inst v, anots::[Annotation]}
+                
 
+                
 -- | Graph that represents calculations on v-dimensional orthotope
-type Gr v = G.Gr (NLabel v) (ELabel v)
+type Gr v s = G.Gr (NLabel v s) ()
+
+newtype StaticID = StaticID String deriving (Eq, Ord, Show, Read)
+
+
+class Arity a where
+  arity :: a -> (Int, Int)
+
+arityI, arityO :: (Arity a) => a -> Int
+arityI = fst.arity
+arityO = snd.arity
+  
 
 data Inst v = 
-  Reduce |
+  Load StaticID |
+  Store StaticID |
+  Reduce ReduceOperation |
   Broadcast |
   Shift (v Int) |
   Arith Arithmetic
 
-
+instance Arity (Inst v) where
+  arity a = case a of
+    Load _    -> (0,1)
+    Store _   -> (1,0)
+    Reduce _  -> (1,1)
+    Broadcast -> (1,1)
+    Shift _   -> (1,1)
+    Arith a   -> arity a
+                   
 data Annotation = Comment String | Balloon
+
 
 data Arithmetic = 
     Add |
@@ -36,23 +60,19 @@ data Arithmetic =
     deriving (Eq, Ord, Show, Read)
 
 
+data ReduceOperation = ReduceMax | ReduceMin | ReduceSum 
 
-arity :: Arithmetic -> (Int, Int) 
+instance Arity Arithmetic where
+  arity a = case a of
+    Add -> (2,1)
+    Sub -> (2,1)
+    Neg -> (1,1)
+    Mul -> (2,1)
+    Div -> (2,1)
+    Inv -> (1,1)
+    Madd -> (3,1)
+    Msub -> (3,1)
+    Nmadd -> (3,1)
+    Nmsub -> (3,1)
+    Sincos -> (1,2)
 
-arity a = case a of
-            Add -> (2,1)
-            Sub -> (2,1)
-            Neg -> (1,1)
-            Mul -> (2,1)
-            Div -> (2,1)
-            Inv -> (1,1)
-            Madd -> (3,1)
-            Msub -> (3,1)
-            Nmadd -> (3,1)
-            Nmsub -> (3,1)
-            Sincos -> (1,2)
-
-arityI, arityO :: Arithmetic -> Int
-arityI = fst.arity
-arityO = snd.arity
-  
