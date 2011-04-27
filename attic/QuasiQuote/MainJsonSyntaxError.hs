6b7ad53ef5131472f3a76e5f3601bf218b23b363
nushio <muranushi@gmail.com>
2011-04-27 17:01:39 +0900

    Syntax Error example separated

--- attic/QuasiQuote/MainJsonSyntaxError.hs
+++ attic/QuasiQuote/MainJsonSyntaxError.hs
@@ -6,4 +6,4 @@ main :: IO ()
 main = do
   let twelve = 12::Int; name = "mr_konn"
   print $ [js| {ident: `twelve, name: `name} |]
-  print $ [js| {ident: `twelve, name: 12} |] --nanashi. This will cause a parse error.
+  print $ [js| {ident: `twelve, name `name } |] -- cause a parse error.
