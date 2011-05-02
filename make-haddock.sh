bd114778514e2a6f7d848ebfcc4d55bf1299ac68
nushio <muranushi@gmail.com>
2011-05-02 23:48:01 +0900

    stop internal

--- make-haddock.sh
+++ make-haddock.sh
@@ -1,3 +1,3 @@
 cabal configure
 cabal hscolour
-cabal haddock --internal '--haddock-options=--source-base=src/ --source-module=src/%{MODULE/./-}.html --source-entity=src/%{MODULE/./-}.html#%N'  --html-location='http://hackage.haskell.org/packages/archive/$pkg/latest/doc/html' 
+cabal haddock '--haddock-options=--source-base=src/ --source-module=src/%{MODULE/./-}.html --source-entity=src/%{MODULE/./-}.html#%N'  --html-location='http://hackage.haskell.org/packages/archive/$pkg/latest/doc/html' 
