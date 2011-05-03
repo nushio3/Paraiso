94c9966cbcf51c8e51eaa6a1d98d71f7fa147252
Takayuki Muranushi <muranushi@gmail.com>
2011-05-03 11:22:42 +0900

    symbol translation database being created

--- Language/Paraiso/Generator/Cpp.hs
+++ Language/Paraiso/Generator/Cpp.hs
@@ -7,9 +7,10 @@ module Language.Paraiso.Generator.Cpp
      Cpp(..)
     ) where
 import Control.Monad
-import Control.Monad.Failure
 import Data.Dynamic
+import Language.Paraiso.Failure
 import Language.Paraiso.Generator
+import Language.Paraiso.OM.Graph
 import System.Directory
 import System.FilePath
 
@@ -18,22 +19,33 @@ data Cpp = Cpp deriving (Eq, Show)
 instance Generator Cpp where
   generate _ pom path = do
     createDirectoryIfMissing True path
-    writeFile (path </> "test.h") "tabula rasa\n"
+    writeFile (path </> "test.h") "he"
 
 instance Symbolable Cpp Dynamic where
   symbolF Cpp dyn = let
-    f :: (Typeable a) => Dynamic -> Maybe a
-    f = fromDynamic
-    xs :: [Dynamic -> Maybe String]
-    xs = [fmap (\x->if x then "true" else "false") . f,
-          fmap (show::Int->String) . f, 
-          fmap (show::Integer->String) . f, 
-          fmap ((++"f").show::Float->String) . f, 
-          fmap (show::Double->String) . f
+    add' :: (Typeable a) => a -> String -> (a->String) 
+        -> (Dynamic -> Maybe String, TypeRep -> Maybe String)
+    add' dummy typename f = (fmap f . fromDynamic, undefined)
+    add ::  (Typeable a) => String -> (a->String) 
+        -> (Dynamic -> Maybe String, TypeRep -> Maybe String)
+    add = add' undefined
+    db :: [(Dynamic -> Maybe String, TypeRep -> Maybe String)]
+    db = [ 
+      add "bool" (\x->if x then "true" else "false"),
+      add "int" (show::Int->String), 
+      add "long long int" (show::Integer->String), 
+      add "float" ((++"f").show::Float->String), 
+      add "double" (show::Double->String)
           ]  
-    ret = msum $ map ($dyn) xs
+    dyndb = map fst db
+    ret = msum $ map ($dyn) dyndb
    in case ret of
       Just str -> return str
       Nothing -> failure $ StringException $ 
                  "Cpp cannot translate symbol of type " ++ show dyn
+  
+instance Symbolable Cpp Name where
+  symbolF Cpp = return . nameStr
+  
+  
   
