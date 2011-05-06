c2ef2674be682d5c759cf170aab998830efce47d
Takayuki Muranushi <muranushi@gmail.com>
2011-05-07 01:35:40 +0900

    New prelude that hides list-specific functions from Prelude and Control.Monad and exports their more generic counterparts from Data.Foldable and Data.Traversable.

--- Language/Paraiso/Prelude.hs
+++ Language/Paraiso/Prelude.hs
@@ -5,18 +5,24 @@
 -}
 
 
+
 module Language.Paraiso.Prelude
   (
-   module Data.Functor,
+   module Control.Monad,
+   module Data.Foldable,
+   module Data.Traversable,
    module NumericPrelude,
    Boolean(..)) where
 
-import Control.Monad
+import Control.Monad hiding 
+    (mapM_, sequence_, forM_, msum, mapM, sequence, forM)
 import Data.Foldable
+import Data.Traversable
 import NumericPrelude hiding 
-    (not, (&&), (||), 
-     fmap, 
-     foldl, foldr, )
+    (not, (&&), (||), Monad, Functor,
+     (>>=), (>>), return, fail, fmap, mapM, mapM_, sequence, sequence_, (=<<), foldl, foldl1, foldr, foldr1, and, or, any, all, sum, product, concat, concatMap, maximum, minimum, elem, notElem
+    )
+    
 import qualified NumericPrelude as Prelude
 
 infixr 3  &&
