dafbe603904e3b2a708221648552c25631be5a71
Takayuki Muranushi <muranushi@gmail.com>
2011-05-08 11:16:54 +0900

    generation counter added. generation double count bug fixed.

--- examples/Life/main.cpp
+++ examples/Life/main.cpp
@@ -15,7 +15,7 @@ int main () {
   Life sim(W, H);
   sim.init();
   int wait = 1000000;
-  for (sim.generation() = 0; sim.generation() < 1000; sim.generation() ++) {
+  for (sim.generation() = 0; sim.generation() < 1500;) {
     string buf; buf.resize((W+1)*H);
     for (int y = 0; y < H/2; ++y) {
       for (int x = 0; x < W; ++x) {
@@ -27,7 +27,7 @@ int main () {
       buf[y*(W+1)+W]='\n';
     }
     system("clear");
-    cout << buf <<endl;
+    cout << buf << "\n" << sim.generation() << endl;
     usleep(wait);
     wait = max(100000, int(wait*0.9));
     sim.proceed();
