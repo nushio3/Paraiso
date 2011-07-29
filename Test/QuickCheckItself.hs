a7f866fe7cb1eeb0f440c5b86da4e0d7c932774c
Takayuki Muranushi <muranushi@gmail.com>
2011-07-29 23:27:11 +0900

    established how to test IO Result

--- Test/QuickCheckItself.hs
+++ Test/QuickCheckItself.hs
@@ -6,11 +6,14 @@ module Test.QuickCheckItself
 
 import Test.Framework  (Test)
 import Test.Framework.Providers.HUnit (testCase)
+import Test.Framework.Providers.API (TestName)
 import Test.QuickCheck
 import Test.HUnit.Lang (assertFailure)
 
 testQuickCheckItself :: Test
-testQuickCheckItself = testCase "length test" testBody
+testQuickCheckItself = testResult "length test" $
+  quickCheckWithResult stdArgs{maxSize=1000, chatty=False} $ 
+    (<=5) . length . first5Vowel . getStringWithA
 
 vowels :: String
 vowels = "AIUEOaiueo"
@@ -26,11 +29,15 @@ instance Arbitrary StringWithA where
     arbitrary = fmap StringWithA $ arbitrary `suchThat` ('a' `elem`)
 
 
-testBody :: IO ()
-testBody = do
-  ret <- quickCheckWithResult stdArgs{maxSize=1000, chatty=False} $ 
-         (<=5) . length . first5Vowel . getStringWithA
+
+testResult :: TestName -> IO Result -> Test
+testResult testName qTest = testCase testName $ do
+  ret <- qTest
   case ret of
     Success _ _ _ -> return ()
     _ -> assertFailure $ output ret
+
+
+
+
 
