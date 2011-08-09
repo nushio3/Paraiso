d6cbe09a44b54be3003ce071a26f65e45f1886b0
Takayuki Muranushi <muranushi@gmail.com>
2011-08-09 10:38:03 +0900

    Option selectable for tests.

--- runtests.hs
+++ runtests.hs
@@ -1,17 +1,20 @@
-import Test.Framework (defaultMain)
+import           Control.Exception (finally)
+import           Control.Monad
+import           Test.Framework (defaultMainWithArgs)
+import qualified Test.Paraiso.Option as Option
 
 -- Actual tests
 import Test.Paraiso.Annotation
-import Test.Paraiso.Claris as C
+import Test.Paraiso.Claris 
 import Test.Paraiso.QuickCheckItself
 import Test.Paraiso.Tensor
 
 
 main :: IO ()
-main = defaultMain 
-  [testQuickCheckItself, 
-   testAnnotation, 
-   testTensor,
-   testClaris]
-
-
+main = 
+  flip defaultMainWithArgs Option.argv 
+    [testQuickCheckItself, 
+     testAnnotation, 
+     testTensor,
+     testClaris]
+    `finally` when Option.help Option.printHelp
