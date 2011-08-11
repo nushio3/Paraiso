6bfeb31ec36a4ee284f5ab950687e57b36fc10d9
Takayuki Muranushi <muranushi@gmail.com>
2011-08-11 17:58:50 +0900

    Lifegame example on REPA

--- attic/Repa/Life.hs
+++ attic/Repa/Life.hs
@@ -10,7 +10,9 @@ import qualified Data.Text.IO as T
 import qualified Data.ListLike as LL
 import qualified Data.ListLike.String as LL
 import qualified Data.ListLike.Text as LL ()
+import           System.Posix.Unistd (usleep)
 
+type Cell = R.Array R.DIM2 Int
 
 ppCell :: R.Array R.DIM2 Bool -> T.Text
 ppCell cell = ret
@@ -20,21 +22,63 @@ ppCell cell = ret
     ret = LL.unlines 
       [ LL.concat [ pat (cell ! (R.Z:.x:.y)) (cell ! (R.Z:.x:.(y+1)))
         | x <- [0 .. w-1]]
-      | y <- [0 .. div h 2-1]]
+      | y <- [0, 2 .. 2*(div h 2) -1]]
 
     pat False False = " "
     pat False True  = "▄"
     pat True  False = "▀"
     pat True  True  = "█"
-sh :: R.DIM2
-sh = R.Z :. 60 :. 30
-
-ar :: R.Array R.DIM2 Int
-ar = R.fromFunction sh (\(R.Z :. x :. y) -> mod (x+y) 5)
 
 
+
+
+initAr :: R.DIM2 -> Cell
+initAr sh = R.fromFunction sh (\x -> if elem x pat then 1 else 0)
+  where
+    origin = let (R.Z :. x :. y) = sh in R.Z :. (div x 2) :. (div y 2)
+    pat = map (R.addDim origin) 
+      [ R.Z :. 1 :. 0 ,
+        R.Z :. 2 :. 0 ,
+        R.Z :. 0 :. 1 ,
+        R.Z :. 1 :. 1 ,
+        R.Z :. 1 :. 2 
+      ]
+
+proceed :: Cell -> Cell
+proceed cell = nextCell
+  where
+    nextCell = R.fromFunction world next
+    
+    next i 
+      | get i == 0 && nbd i == 3                  = 1
+      | get i == 1 && (nbd i == 2 || nbd i ==3)   = 1
+      | otherwise                                 = 0
+        
+    world = R.extent cell
+    get i = case R.safeIndex cell i of
+      Just x  -> x
+      Nothing -> 0
+
+    nbd i = sum $ map (get . R.addDim i) adjs
+
+    adjs = 
+      [ R.Z :. -1 :. -1,
+        R.Z :.  0 :. -1,
+        R.Z :.  1 :. -1,
+        R.Z :. -1 :.  0,
+        R.Z :.  1 :.  0,
+        R.Z :. -1 :.  1,
+        R.Z :.  0 :.  1,
+        R.Z :.  1 :.  1
+      ]
 
 main :: IO ()
 main = do
-  putStrLn $ R.showShape sh
-  T.putStrLn $ ppCell $ R.map (/=0) ar
+  loop (initAr $ R.Z :. 128 :. 128)
+
+
+loop :: Cell -> IO ()
+loop cell = do
+  T.putStrLn $ ppCell $ R.map (/=0) cell 
+  usleep 10000
+  loop (R.force $ proceed cell)
