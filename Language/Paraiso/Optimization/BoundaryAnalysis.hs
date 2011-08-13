edb8006c45888d802744d7eaca847452dcd54d62
nushio <muranushi@gmail.com>
2011-08-13 21:18:38 +0900

    boundary analysis is implemented.

--- Language/Paraiso/Optimization/BoundaryAnalysis.hs
+++ Language/Paraiso/Optimization/BoundaryAnalysis.hs
@@ -15,7 +15,6 @@ import           Language.Paraiso.Interval
 import           Language.Paraiso.OM.DynValue as DVal
 import           Language.Paraiso.OM.Graph
 import           Language.Paraiso.OM.Realm as Realm
-import           Language.Paraiso.Optimization.Graph
 import           Language.Paraiso.PiSystem
 import           Language.Paraiso.Prelude
 import           Language.Paraiso.Tensor
@@ -33,38 +32,49 @@ boundaryAnalysis graph = imap update graph
     memoAnot = V.generate (FGL.noNodes graph) calcAnot
 
     calcAnot i = case selfNode of
-      NValue v _            -> if isGlobal v 
-                               then infiniteValid graph 
-                               else anot (uniquePre i)
-      NInst (Imm _) _       -> infiniteValid graph
-      NInst (Load _) _      -> fullValid graph
-      NInst (Store _) _     -> anot (uniquePre i)
-      NInst (Reduce _) _    -> infiniteValid graph
-      NInst (Broadcast) _   -> infiniteValid graph
-      NInst (Shift _) _     -> fullValid graph `intersection` anot (uniquePre i)
-      NInst (LoadIndex _) _ -> anot (uniquePre i)
-      NInst (Arith _) _     -> merge i
-      _                     -> error "unsupported path in boundary analysis"
+      NValue v _           -> if isGlobal v 
+                              then infinite
+                              else preAnot
+      NInst (Imm _)_       -> infinite
+      NInst (Load _)_      -> full
+      NInst (Store _)_     -> preAnot
+      NInst (Reduce _)_    -> infinite
+      NInst (Broadcast)_   -> infinite
+      NInst (Shift v)_     -> full `intersection` shiftPreBy v 
+      NInst (LoadIndex _)_ -> preAnot
+      NInst (Arith _)_     -> mergedAnot
       where
         self0 = FGL.lab graph i
-        pre0  = FGL.lab graph =<<(listToMaybe $ FGL.pre graph i) 
-        suc0  = FGL.lab graph =<<(listToMaybe $ FGL.suc graph i) 
 
         selfNode = case self0 of   
           Just x -> x
           _      -> error $ "node[" ++ show i ++ "] disappeared"
 
-        uniquePre i = case FGL.pre graph i of         
-          [i'] -> i'
-          xs    -> error $ "node[" ++ show i ++ "] only 1 pre expected : actually " ++ show (length xs)
-
         isGlobal v = case v of
           DVal.DynValue Realm.Global _ -> True
           _                            -> False
 
-        merge i = case (FGL.pre graph i) of
+        full = fullValid graph
+        infinite = infiniteValid graph
+
+        preAnot = case FGL.pre graph i of         
+          [i'] -> anot i'
+          xs    -> error $ "node[" ++ show i ++ "] only 1 pre expected : actually " ++ show (length xs)
+
+        mergedAnot = case (FGL.pre graph i) of
           [] -> error $ "arith node[" ++ show i ++ "] has 0 pre"
           xs -> foldl1 intersection $ map anot $ xs
+
+        shiftPreBy v = compose $ \j -> shiftIntervalBy (v!j) (preAnot ! j)
+
+        shiftIntervalBy x (Interval x1 x2) = Interval (add x x1) (add x x2)
+        shiftIntervalBy _ Empty            = error "empty interval raised!"
+        
+        add x nby = case nby of
+          NegaInfinity    -> NegaInfinity
+          LowerBoundary y -> LowerBoundary $ x+y
+          UpperBoundary y -> UpperBoundary $ x+y
+          PosiInfinity    -> PosiInfinity
 
 fullValid :: (Vector v, Additive.C g) => Graph v g a -> v (Interval (NearBoundary g))
 fullValid _ = compose (\_ -> Interval (LowerBoundary Additive.zero) (UpperBoundary Additive.zero))
