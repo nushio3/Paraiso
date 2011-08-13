cae12c7615dced75df964a09578560a1687fca30
nushio <muranushi@gmail.com>
2011-08-14 01:06:06 +0900

    prettier prettyprint

--- Language/Paraiso/OM/PrettyPrint.hs
+++ Language/Paraiso/OM/PrettyPrint.hs
@@ -17,24 +17,31 @@ import           Language.Paraiso.Prelude
 import           Language.Paraiso.Tensor
 
 prettyPrint :: (Vector v, Show (v g), Show a) => OM v g a -> T.Text
-prettyPrint om = LL.unlines [nameText om,staticList,kernelList]
+prettyPrint om 
+  = LL.unlines 
+    [ "OM name: " ++ nameText om,
+      "**** Static Variables ****",
+      staticList,
+      "**** Kernels ****",
+      kernelList
+    ]
   where
     staticList = LL.unlines $ V.toList $ V.map showT $ staticValues $ setup om
     kernelList = LL.unlines $ V.toList $ V.map ppKern $ kernels om
 
-    ppKern kern = LL.unlines $ ["", "**** " ++ nameText kern ++ " ****"] ++ body (dataflow kern)
+    ppKern kern = LL.unlines $ ["", "Kernel name: " ++ nameText kern] ++ body (dataflow kern)
     body graph = map ppCon $ map (FGL.context graph) $ FGL.nodes graph
     
     ppCon (input, idx, nodeLabel, output) 
       = LL.unwords
         [ showT idx, 
           showT nodeLabel,
-          "<-",
-          ppEdges input,
-          "->",
-          ppEdges output
+          ppEdges "<-" input,
+          ppEdges "->" output
         ]
-    ppEdges = LL.unwords . map ppEdge 
+    ppEdges symbol xs 
+      | length xs == 0 = ""
+      | otherwise      = LL.unwords $ symbol : map ppEdge xs
     ppEdge (e, i) = case e of
       EUnord -> showT i
       EOrd x -> "(" ++ showT x ++ ")" ++ showT i
