4767fe54daae0cf92cc774949eab2ecbeac08683
nushio <muranushi@gmail.com>
2011-08-13 13:58:35 +0900

    allocation is added as an annotation.

---
+++ Language/Paraiso/Annotation/Allocation.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE DeriveDataTypeable, NoImplicitPrelude #-}
+{-# OPTIONS -Wall #-}
+-- | An 'Annotation' that selects whether the data should be 
+-- stored globally on memory or to be calculated.
+
+module Language.Paraiso.Annotation.Allocation (
+  Allocation(..)
+  ) where
+
+import Data.Dynamic
+import Language.Paraiso.Prelude
+
+data Allocation 
+  = Manifest 
+  | Delayed 
+  deriving (Eq, Show, Typeable)
