6b46bbf1430a89d69d07612dc5054f112c17700a
nushio <muranushi@gmail.com>
2011-08-14 11:26:57 +0900

    boundary analysis resulting in error

--- Language/Paraiso/Optimization.hs
+++ Language/Paraiso/Optimization.hs
@@ -8,16 +8,26 @@ module Language.Paraiso.Optimization (
   OptimizationLevel(..)
   ) where
 
+import qualified Algebra.Additive            as Additive
+import           Data.Typeable
 import           Language.Paraiso.Annotation
+import           Language.Paraiso.Annotation.Boundary
+import           Language.Paraiso.Interval
 import           Language.Paraiso.OM
+import           Language.Paraiso.Optimization.BoundaryAnalysis
 import           Language.Paraiso.Optimization.DecideAllocation
 import           Language.Paraiso.Optimization.Graph
 import           Language.Paraiso.Optimization.Identity
 import           Language.Paraiso.Prelude
+import           Language.Paraiso.Tensor (Vector)
 
-optimize :: OptimizationLevel -> OM v g Annotation -> OM v g Annotation
+optimize :: (Vector v, 
+             Additive.C g, 
+             Ord g, 
+             Typeable (v (Interval (NearBoundary g)))) => 
+            OptimizationLevel -> OM v g Annotation -> OM v g Annotation
 optimize level = case level of
-  O0 -> gmap identity . gmap decideAllocation
+  O0 -> gmap identity . gmap boundaryAnalysis . gmap decideAllocation
   _  -> optimize O0
 
 data OptimizationLevel 
