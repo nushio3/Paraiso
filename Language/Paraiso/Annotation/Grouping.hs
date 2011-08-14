66dd2c8002f45d9595c1eb06da33f04e46a9793c
nushio <muranushi@gmail.com>
2011-08-14 14:14:53 +0900

    grouping data type

---
+++ Language/Paraiso/Annotation/Grouping.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE DeriveDataTypeable, NoImplicitPrelude #-}
+{-# OPTIONS -Wall #-}
+-- | An 'Annotation' that labels certain group of Manifest nodes
+-- that can safely be accessed simultaneously
+
+module Language.Paraiso.Annotation.Grouping (
+  KernelWriteGroup(..), 
+  OMWriteGroup(..)
+  ) where
+
+import Data.Dynamic
+import Language.Paraiso.Prelude
+
+-- | Write grouping, continuously numbered from [0 ..] .
+-- The numbering starts from 0 for each kerenel in a Orthotope Machine.
+data KernelWriteGroup
+  = KernelWriteGroup Int
+  deriving (Eq, Show, Typeable)
+
+-- | Write grouping, continuously numbered from [0 ..] .
+-- The numbering is unique in one Orthotope Machine.
+data OMWriteGroup
+  = OMWriteGroup Int
+  deriving (Eq, Show, Typeable)
+
