6d139967437218af0d261fe7b51d305e9b9d8e9d
Takayuki Muranushi <muranushi@gmail.com>
2011-08-21 17:18:51 +0900

    new example codes for lifegame

--- examples/Life-exampled/dist/Life.cpp
+++ examples/Life-exampled/dist/Life.cpp
@@ -1,190 +1,184 @@
+
 #include "Life.hpp"
-using namespace std;
 
-void Life::init () {
-std::vector<int> a67(size());
-int a70;
-int a73;
 
-for (int i = 0 ; i < size() ; ++i) {
-const int a1_0_0 = ((i) % size0());
-const int a3_0_0 = ((i / size0()) % size1());
-const int a5_0_0 = 1;
-const bool a7_0_0 = (a1_0_0 == a5_0_0);
-const int a9_0_0 = 0;
-const bool a11_0_0 = (a3_0_0 == a9_0_0);
-const bool a13_0_0 = (a7_0_0 && a11_0_0);
-const int a15_0_0 = 2;
-const bool a17_0_0 = (a1_0_0 == a15_0_0);
-const int a19_0_0 = 0;
-const bool a21_0_0 = (a3_0_0 == a19_0_0);
-const bool a23_0_0 = (a17_0_0 && a21_0_0);
-const bool a25_0_0 = (a13_0_0 || a23_0_0);
-const int a27_0_0 = 0;
-const bool a29_0_0 = (a1_0_0 == a27_0_0);
-const int a31_0_0 = 1;
-const bool a33_0_0 = (a3_0_0 == a31_0_0);
-const bool a35_0_0 = (a29_0_0 && a33_0_0);
-const bool a37_0_0 = (a25_0_0 || a35_0_0);
-const int a39_0_0 = 1;
-const bool a41_0_0 = (a1_0_0 == a39_0_0);
-const int a43_0_0 = 1;
-const bool a45_0_0 = (a3_0_0 == a43_0_0);
-const bool a47_0_0 = (a41_0_0 && a45_0_0);
-const bool a49_0_0 = (a37_0_0 || a47_0_0);
-const int a51_0_0 = 1;
-const bool a53_0_0 = (a1_0_0 == a51_0_0);
-const int a55_0_0 = 2;
-const bool a57_0_0 = (a3_0_0 == a55_0_0);
-const bool a59_0_0 = (a53_0_0 && a57_0_0);
-const bool a61_0_0 = (a49_0_0 || a59_0_0);
-const int a63_0_0 = 1;
-const int a65_0_0 = 0;
-a67[i] = (a61_0_0 ? a63_0_0 : a65_0_0);
 
+
+template <class T> T broadcast (const T& x) {
+  return x;
+}
+template <class T> T reduce_sum (const std::vector<T> &xs) {
+  T ret = 0;
+  for (int i = 0; i < xs.size(); ++i) ret+=xs[i];
+  return ret;
+}
+template <class T> T reduce_min (const std::vector<T> &xs) {
+  T ret = xs[0];
+  for (int i = 1; i < xs.size(); ++i) ret=std::min(ret,xs[i]);
+  return ret;
+}
+template <class T> T reduce_max (const std::vector<T> &xs) {
+  T ret = xs[0];
+  for (int i = 1; i < xs.size(); ++i) ret=std::max(ret,xs[i]);
+  return ret;
 }
 
-for (int i = 0 ; i < size() ; ++i) {
-cell()[i] = a67[i];
+/*
+lowerMargin = (Vec :~ 1) :~ 1
+upperMargin = (Vec :~ 1) :~ 1
+*/
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void Life::Life_sub_0 (std::vector<int>  & a99)  {
+/*
+lowerMargin = (Vec :~ 0) :~ 0
+upperMargin = (Vec :~ 0) :~ 0
+*/
+for(int i = 0; (i) < (16900) ; ++(i)){
+int addr_origin = i;
+int a1_0_0 = ((i) % (130)) - (1);
+int a3_0_0 = ((i) / (130)) - (1);
+int a5_0_0 = 128;
+int a7_0_0 = 128;
+int a9_0_0 = 2;
+int a11_0_0 = (a5_0_0) / (a9_0_0);
+int a13_0_0 = 2;
+int a15_0_0 = (a7_0_0) / (a13_0_0);
+int a17_0_0 = (a1_0_0) - (a11_0_0);
+int a19_0_0 = 1;
+bool a21_0_0 = (a17_0_0) == (a19_0_0);
+int a23_0_0 = (a3_0_0) - (a15_0_0);
+int a25_0_0 = 0;
+bool a27_0_0 = (a23_0_0) == (a25_0_0);
+bool a29_0_0 = (a21_0_0) && (a27_0_0);
+int a31_0_0 = (a1_0_0) - (a11_0_0);
+int a33_0_0 = 2;
+bool a35_0_0 = (a31_0_0) == (a33_0_0);
+int a37_0_0 = (a3_0_0) - (a15_0_0);
+int a39_0_0 = 0;
+bool a41_0_0 = (a37_0_0) == (a39_0_0);
+bool a43_0_0 = (a35_0_0) && (a41_0_0);
+bool a45_0_0 = (a29_0_0) || (a43_0_0);
+int a47_0_0 = (a1_0_0) - (a11_0_0);
+int a49_0_0 = 0;
+bool a51_0_0 = (a47_0_0) == (a49_0_0);
+int a53_0_0 = (a3_0_0) - (a15_0_0);
+int a55_0_0 = 1;
+bool a57_0_0 = (a53_0_0) == (a55_0_0);
+bool a59_0_0 = (a51_0_0) && (a57_0_0);
+bool a61_0_0 = (a45_0_0) || (a59_0_0);
+int a63_0_0 = (a1_0_0) - (a11_0_0);
+int a65_0_0 = 1;
+bool a67_0_0 = (a63_0_0) == (a65_0_0);
+int a69_0_0 = (a3_0_0) - (a15_0_0);
+int a71_0_0 = 1;
+bool a73_0_0 = (a69_0_0) == (a71_0_0);
+bool a75_0_0 = (a67_0_0) && (a73_0_0);
+bool a77_0_0 = (a61_0_0) || (a75_0_0);
+int a79_0_0 = (a1_0_0) - (a11_0_0);
+int a81_0_0 = 1;
+bool a83_0_0 = (a79_0_0) == (a81_0_0);
+int a85_0_0 = (a3_0_0) - (a15_0_0);
+int a87_0_0 = 2;
+bool a89_0_0 = (a85_0_0) == (a87_0_0);
+bool a91_0_0 = (a83_0_0) && (a89_0_0);
+bool a93_0_0 = (a77_0_0) || (a91_0_0);
+int a95_0_0 = 1;
+int a97_0_0 = 0;
+((a99)[addr_origin]) = ((a93_0_0) ? (a95_0_0) : (a97_0_0));
 }
-
-int reduce_69 = a67[0];
-for (int i = 1 ; i < size(); ++i) {
-reduce_69 = (reduce_69 + a67[i]);
 }
-
-a70 = reduce_69;
-
-
-population() = a70;
-
-
-a73 = 0;
-
-
-generation() = a73;
-
-
-
-return;
+void Life::Life_sub_1 (const std::vector<int>  & a99, int & a102, int & a105)  {
+(a102) = (reduce_sum(a99));
+(a105) = (0);
 }
-
-void Life::proceed () {
-std::vector<int> a1(size());
-int a3;
-std::vector<int> a67(size());
-int a69;
-int a72;
-int a74;
-
-for (int i = 0 ; i < size() ; ++i) {
-a1[i] = cell()[i];
-
+void Life::Life_sub_2 (const std::vector<int>  & a1, std::vector<int>  & a67)  {
+/*
+lowerMargin = (Vec :~ 1) :~ 1
+upperMargin = (Vec :~ 1) :~ 1
+*/
+for(int i = 0; (i) < (16384) ; ++(i)){
+int addr_origin = ((((i) % (128)) + (1)) * (1)) + ((((i) / (128)) + (1)) * (130));
+int a1_m1_m1 = (a1)[(addr_origin) + (-131)];
+int a1_0_m1 = (a1)[(addr_origin) + (-130)];
+int a1_1_m1 = (a1)[(addr_origin) + (-129)];
+int a1_m1_0 = (a1)[(addr_origin) + (-1)];
+int a1_0_0 = (a1)[(addr_origin) + (0)];
+int a1_1_0 = (a1)[(addr_origin) + (1)];
+int a1_m1_1 = (a1)[(addr_origin) + (129)];
+int a1_0_1 = (a1)[(addr_origin) + (130)];
+int a1_1_1 = (a1)[(addr_origin) + (131)];
+int a5_0_0 = a1_1_1;
+int a7_0_0 = a1_0_1;
+int a9_0_0 = a1_m1_1;
+int a11_0_0 = a1_1_0;
+int a13_0_0 = a1_m1_0;
+int a15_0_0 = a1_1_m1;
+int a17_0_0 = a1_0_m1;
+int a19_0_0 = a1_m1_m1;
+int a21_0_0 = (a5_0_0) + (a7_0_0);
+int a23_0_0 = (a21_0_0) + (a9_0_0);
+int a25_0_0 = (a23_0_0) + (a11_0_0);
+int a27_0_0 = (a25_0_0) + (a13_0_0);
+int a29_0_0 = (a27_0_0) + (a15_0_0);
+int a31_0_0 = (a29_0_0) + (a17_0_0);
+int a33_0_0 = (a31_0_0) + (a19_0_0);
+int a35_0_0 = 0;
+bool a37_0_0 = (a1_0_0) == (a35_0_0);
+int a39_0_0 = 3;
+bool a41_0_0 = (a33_0_0) == (a39_0_0);
+bool a43_0_0 = (a37_0_0) && (a41_0_0);
+int a45_0_0 = 1;
+bool a47_0_0 = (a1_0_0) == (a45_0_0);
+int a49_0_0 = 2;
+bool a51_0_0 = (a33_0_0) >= (a49_0_0);
+int a53_0_0 = 3;
+bool a55_0_0 = (a33_0_0) <= (a53_0_0);
+bool a57_0_0 = (a51_0_0) && (a55_0_0);
+bool a59_0_0 = (a47_0_0) && (a57_0_0);
+bool a61_0_0 = (a43_0_0) || (a59_0_0);
+int a63_0_0 = 1;
+int a65_0_0 = 0;
+((a67)[addr_origin]) = ((a61_0_0) ? (a63_0_0) : (a65_0_0));
 }
-
-a3 = generation();
-
-
-for (int i = 0 ; i < size() ; ++i) {
-const int a5_0_0 = a1[((((i) % size0()) + 1 + size0())%size0()) + size0() * (((((i / size0()) % size1()) + 1 + size1())%size1()))];
-const int a7_0_0 = a1[((((i) % size0()) + 0 + size0())%size0()) + size0() * (((((i / size0()) % size1()) + 1 + size1())%size1()))];
-const int a9_0_0 = a1[((((i) % size0()) + -1 + size0())%size0()) + size0() * (((((i / size0()) % size1()) + 1 + size1())%size1()))];
-const int a11_0_0 = a1[((((i) % size0()) + 1 + size0())%size0()) + size0() * (((((i / size0()) % size1()) + 0 + size1())%size1()))];
-const int a13_0_0 = a1[((((i) % size0()) + -1 + size0())%size0()) + size0() * (((((i / size0()) % size1()) + 0 + size1())%size1()))];
-const int a15_0_0 = a1[((((i) % size0()) + 1 + size0())%size0()) + size0() * (((((i / size0()) % size1()) + -1 + size1())%size1()))];
-const int a17_0_0 = a1[((((i) % size0()) + 0 + size0())%size0()) + size0() * (((((i / size0()) % size1()) + -1 + size1())%size1()))];
-const int a19_0_0 = a1[((((i) % size0()) + -1 + size0())%size0()) + size0() * (((((i / size0()) % size1()) + -1 + size1())%size1()))];
-const int a21_0_0 = (a5_0_0 + a7_0_0);
-const int a23_0_0 = (a21_0_0 + a9_0_0);
-const int a25_0_0 = (a23_0_0 + a11_0_0);
-const int a27_0_0 = (a25_0_0 + a13_0_0);
-const int a29_0_0 = (a27_0_0 + a15_0_0);
-const int a31_0_0 = (a29_0_0 + a17_0_0);
-const int a33_0_0 = (a31_0_0 + a19_0_0);
-const int a35_0_0 = 0;
-const bool a37_0_0 = (a1[i] == a35_0_0);
-const int a39_0_0 = 3;
-const bool a41_0_0 = (a33_0_0 == a39_0_0);
-const bool a43_0_0 = (a37_0_0 && a41_0_0);
-const int a45_0_0 = 1;
-const bool a47_0_0 = (a1[i] == a45_0_0);
-const int a49_0_0 = 2;
-const bool a51_0_0 = (a33_0_0 >= a49_0_0);
-const int a53_0_0 = 3;
-const bool a55_0_0 = (a33_0_0 <= a53_0_0);
-const bool a57_0_0 = (a51_0_0 && a55_0_0);
-const bool a59_0_0 = (a47_0_0 && a57_0_0);
-const bool a61_0_0 = (a43_0_0 || a59_0_0);
-const int a63_0_0 = 1;
-const int a65_0_0 = 0;
-a67[i] = (a61_0_0 ? a63_0_0 : a65_0_0);
-
 }
-
-int reduce_68 = a67[0];
-for (int i = 1 ; i < size(); ++i) {
-reduce_68 = (reduce_68 + a67[i]);
+void Life::Life_sub_3 (const int & a3, const std::vector<int>  & a67, int & a69, int & a74)  {
+int a3_0_0 = a3;
+(a69) = (reduce_sum(a67));
+int a72_0_0 = 1;
+(a74) = ((a3_0_0) + (a72_0_0));
 }
-
-a69 = reduce_68;
-
-
-population() = a69;
-
-
-a72 = 1;
-
-
-a74 = (a3 + a72);
-
-
-generation() = a74;
-
-
-for (int i = 0 ; i < size() ; ++i) {
-cell()[i] = a67[i];
-
+void Life::init ()  {
+Life_sub_0(manifest_0_99);
+Life_sub_1(manifest_0_99, manifest_0_102, manifest_0_105);
+(static_2_cell) = (manifest_0_99);
+(static_0_population) = (manifest_0_102);
+(static_1_generation) = (manifest_0_105);
 }
-
-
-return;
+void Life::proceed ()  {
+Life_sub_2(static_2_cell, manifest_1_67);
+Life_sub_3(static_1_generation, manifest_1_67, manifest_1_69, manifest_1_74);
+(static_0_population) = (manifest_1_69);
+(static_1_generation) = (manifest_1_74);
+(static_2_cell) = (manifest_1_67);
 }
-
-void Life::shift_x () {
-std::vector<int> a1(size());
-
-for (int i = 0 ; i < size() ; ++i) {
-a1[i] = cell()[i];
-
-}
-
-for (int i = 0 ; i < size() ; ++i) {
-const int a3_0_0 = a1[((((i) % size0()) + -1 + size0())%size0()) + size0() * (((((i / size0()) % size1()) + 0 + size1())%size1()))];
-cell()[i] = a3_0_0;
-
-}
-
-
-return;
-}
-
-void Life::shift_y () {
-std::vector<int> a1(size());
-
-for (int i = 0 ; i < size() ; ++i) {
-a1[i] = cell()[i];
-
-}
-
-for (int i = 0 ; i < size() ; ++i) {
-const int a3_0_0 = a1[((((i) % size0()) + 0 + size0())%size0()) + size0() * (((((i / size0()) % size1()) + -1 + size1())%size1()))];
-cell()[i] = a3_0_0;
-
-}
-
-
-return;
-}
-
 
--- examples/Life-exampled/dist/Life.hpp
+++ examples/Life-exampled/dist/Life.hpp
@@ -1,34 +1,64 @@
+#pragma once
+
+
 #include <algorithm>
+
 #include <cmath>
+
 #include <vector>
 
 
+/*
+lowerMargin = (Vec :~ 1) :~ 1
+upperMargin = (Vec :~ 1) :~ 1
+*/
 class Life{
-private:
-int size0_;
-int size1_;
-int population_;
-int generation_;
-std::vector<int> cell_;
+public: int static_0_population;
+public: int static_1_generation;
+public: std::vector<int>  static_2_cell;
+public: std::vector<int>  manifest_0_99;
+public: int manifest_0_102;
+public: int manifest_0_105;
+public: std::vector<int>  manifest_1_67;
+public: int manifest_1_69;
+public: int manifest_1_74;
+public:  Life () : static_0_population(memorySize()),static_1_generation(memorySize()),static_2_cell(memorySize()),manifest_0_99(memorySize()),manifest_0_102(memorySize()),manifest_0_105(memorySize()),manifest_1_67(memorySize()),manifest_1_69(memorySize()),manifest_1_74(memorySize()) {
 
-public:
-const int size() const { return size0() * size1(); }
-const int &size0() const { return size0_ ; }
-const int &size1() const { return size1_ ; }
-const int &population() const { return population_ ; }
-const int &generation() const { return generation_ ; }
-const std::vector<int> &cell() const { return cell_ ; }
-
-public:
-int &population() { return population_ ; }
-int &generation() { return generation_ ; }
-std::vector<int> &cell() { return cell_ ; }
-
-public:
-Life ( int size0,int size1 ): size0_(size0),size1_(size1),cell_(size()){}
-void init ();
-void proceed ();
-void shift_x ();
-void shift_y ();
-
+}
+public: int size ()  {
+return 16384;
+}
+public: int size0 ()  {
+return 128;
+}
+public: int size1 ()  {
+return 128;
+}
+public: int lowerMargin0 ()  {
+return 1;
+}
+public: int lowerMargin1 ()  {
+return 1;
+}
+public: int upperMargin0 ()  {
+return 1;
+}
+public: int upperMargin1 ()  {
+return 1;
+}
+public: int memorySize ()  {
+return 16900;
+}
+public: int memorySize0 ()  {
+return 130;
+}
+public: int memorySize1 ()  {
+return 130;
+}
+public: void Life_sub_0 (std::vector<int>  & a99) ;
+public: void Life_sub_1 (const std::vector<int>  & a99, int & a102, int & a105) ;
+public: void Life_sub_2 (const std::vector<int>  & a1, std::vector<int>  & a67) ;
+public: void Life_sub_3 (const int & a3, const std::vector<int>  & a67, int & a69, int & a74) ;
+public: void init () ;
+public: void proceed () ;
 };
