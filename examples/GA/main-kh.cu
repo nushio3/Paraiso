482dac18247884d0e51c01a8f0e30c8d99b5a028
Takayuki Muranushi <muranushi@gmail.com>
2011-10-16 23:34:41 +0900

    changing the code for benchmarking

--- examples/GA/main-kh.cu
+++ examples/GA/main-kh.cu
@@ -8,6 +8,7 @@
 #include <thrust/host_vector.h>
 #include <unistd.h>
 
+#include "get_time.h"
 #include "Hydro.hpp"
 
 using namespace std;
@@ -15,8 +16,7 @@ using namespace std;
 const int antiAlias = 1;
 int W,H;
 
-void dump (string fn, Hydro &sim) {
-  ofstream ofs (fn.c_str()); 
+void check (Hydro &sim) {
   thrust::host_vector<float> dens, vx, vy, p;
 
   dens = sim.static_7_density;
@@ -24,22 +24,37 @@ void dump (string fn, Hydro &sim) {
   vy = sim.static_9_velocity1;
   p = sim.static_10_pressure;
 
+  bool hasNan = false;
+  const float vThre = 1.0;
+  int vxPosiCnt = 0, vxNegaCnt = 0;
+  int vyPosiCnt = 0, vyNegaCnt = 0;
+  
   for (int iy = antiAlias/2; iy < H; iy+=antiAlias) {
     for (int ix = antiAlias/2; ix < W; ix+=antiAlias) {
       double x = sim.static_3_dR0 * (ix+0.5);
       double y = sim.static_4_dR1 * (iy+0.5);
       int i = sim.memorySize0() * (iy+sim.lowerMargin1()) + ix + sim.lowerMargin0();
-      ofs << x << " " << y << " "
-          << dens[i] << " "
-          << vx[i] << " "
-          << vy[i] << " "
-          << p[i] << endl;
+      hasNan = hasNan || not
+	(isfinite(dens[i]) &&
+	 isfinite(vx[i]) && 
+	 isfinite(vy[i]) && 
+	 isfinite(p[i]) );
+      if (vx[i] >  vThre) ++vxPosiCnt; 
+      if (vx[i] < -vThre) ++vxNegaCnt; 
+      if (vy[i] >  vThre) ++vyPosiCnt; 
+      if (vy[i] < -vThre) ++vyNegaCnt; 
     }
-    ofs << endl;
   }
+  cerr << "nan? " << hasNan << endl;
+  cerr << "velocity matrix" << endl;
+  cerr <<
+    vxPosiCnt<<"\t" <<
+    vxNegaCnt<<"\t" <<
+    vyPosiCnt<<"\t" <<
+    vyNegaCnt<<endl;
 }
 
-void simulate () {
+int simulate () {
   Hydro sim;
   W = sim.size0();
   H = sim.size1();
@@ -51,9 +66,6 @@ void simulate () {
   sim.static_3_dR0 = sim.static_5_extent0 / W;
   sim.static_4_dR1 = sim.static_6_extent1 / H;
   sim.init();
-  char buf[256];
-  sprintf(buf, "mkdir -p output-g%d", antiAlias);
-  system(buf);
   int ctr = 0;
   while (ctr <= 1) {
     double t = sim.static_1_time;
@@ -61,11 +73,11 @@ void simulate () {
     if (!isfinite(t)) return -1;
     sim.proceed();
     if (t > 1.0 * ctr) {
-      sprintf(buf, "output-g%d/snapshot%04d.txt", antiAlias, ctr);
-      dump(buf, sim);
+      check(sim);
       ++ctr;
     }
   }
+  return 0;
 }
 
 
