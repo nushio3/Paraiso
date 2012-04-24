188e2082a918e0bf3937dab0ada4f44bc0d10ce6
Takayuki Muranushi <muranushi@gmail.com>
2012-04-24 20:37:14 +0900

    update linear wave sample

--- attic/linearWave/sample.cpp
+++ attic/linearWave/sample.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -6,11 +7,15 @@ using namespace std;
 
 int main () {
   const int N = 100;
-  vector<double> f(N),u(N);
+  vector<double> f(N),u(N), new_f(N),new_u(N);
   const double c = 1.0;  
-  const double dx = 1.0 / N;
-  const double dt = 0.5 * dx / c;
-  for (int i = 0; i < N/3; ++i) {f[i] = 1.0;}
+  const double dx = 2 * 3.1415926536 / N;
+  const double dt = 1.0 * dx / c;
+  for (int i = 0; i < N; ++i) {
+    double x = dx*(i+0.5);
+    f[i] = sin(x);
+    u[i] = cos(3*x);
+  }
 
   int ctr = 0;
   for (double t = 0; t < 3.0; t+=dt) {
@@ -23,13 +28,15 @@ int main () {
       ctr++;
     }
     for (int i = 0; i < N; ++i) {
-      f[i] += dt * u[i];
+      new_f[i] = f[i] + dt * u[i];
     }
+    swap(new_f,f);
 
     for (int i = 0; i < N; ++i) {
       int im = (i+N-1)%N;
       int ip = (i+1)%N;
-      u[i] -= dt*c*c/dx/dx*(f[im]+f[ip]-2*f[i]);
+      new_u[i] = u[i] + dt*c*c/dx/dx*(f[im]+f[ip]-2*f[i]);
     }
+    swap(new_u,u);
   }
 }
