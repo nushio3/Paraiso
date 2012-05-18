0cd4d5bc34440ee106ce3d567e7153b6be254df3
Takayuki Muranushi <muranushi@gmail.com>
2012-05-18 15:04:39 +0900

    I encourage you to buy as many copies as you can afford!  --- Miran Lipovača, th author of "Learn You a Haskell for Great Good!"

--- make-haddock.sh
+++
@@ -1,3 +0,0 @@
-cabal configure
-cabal hscolour
-cabal haddock '--haddock-options=--source-base=src/ --source-module=src/%{MODULE/./-}.html --source-entity=src/%{MODULE/./-}.html#%N'  --html-location='http://hackage.haskell.org/packages/archive/$pkg/latest/doc/html' 
