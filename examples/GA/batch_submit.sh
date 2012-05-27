9f4021578ebe88bc86bea87830a5c6a9fcecbdce
Takayuki Muranushi <muranushi@gmail.com>
2012-05-27 10:12:51 +0900

    minor edit in submitting script

--- examples/GA/batch_submit.sh
+++ examples/GA/batch_submit.sh
@@ -4,4 +4,4 @@ rm GAATCT.e*
 rm GAATCT.o*
 mv GAATCTsub.*  /work0/t2g-ppc-all/nushio/sub-log
 ./batch_submit.rb | bash
-t2sub -N GAATCTsub -q G  -W group_list=t2g-ppc-all  -l select=1:gpus=1:mem=21gb -l walltime=01:48:00 ./batch_submit.sh
+#t2sub -N GAATCTsub -q G  -W group_list=t2g-ppc-all  -l select=1:gpus=1:mem=21gb -l walltime=03:00:00 ./batch_submit.sh
