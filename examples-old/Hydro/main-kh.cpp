f02f60723adac503100cde3722c0ec0b9fb6cadf
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-05-31 17:14:25 +0900

    Hydro example is running again!

--- examples-old/Hydro/main-kh.cpp
+++ examples-old/Hydro/main-kh.cpp
@@ -18,14 +18,13 @@ void dump (string fn, Hydro &sim) {
   ofstream ofs (fn.c_str()); 
   for (int iy = antiAlias/2; iy < H; iy+=antiAlias) {
     for (int ix = antiAlias/2; ix < W; ix+=antiAlias) {
-      double x = sim.static_3_dR0 * (ix+0.5);
-      double y = sim.static_4_dR1 * (iy+0.5);
-      int i = sim.memorySize0() * (iy+sim.lowerMargin1()) + ix + sim.lowerMargin0();
+      double x = sim.dR0() * (ix+0.5);
+      double y = sim.dR1() * (iy+0.5);
       ofs << x << " " << y << " "
-          << sim.static_7_density[i] << " "
-          << sim.static_8_velocity0[i] << " "
-          << sim.static_9_velocity1[i] << " "
-          << sim.static_10_pressure[i] << endl;
+          << sim.density(ix,iy) << " "
+          << sim.velocity0(ix,iy) << " "
+          << sim.velocity1(ix,iy) << " "
+          << sim.pressure(ix,iy) << endl;
     }
     ofs << endl;
   }
@@ -33,23 +32,23 @@ void dump (string fn, Hydro &sim) {
 
 int main () {
   Hydro sim;
-  W = sim.size0();
-  H = sim.size1();
+  W = sim.om_size_0();
+  H = sim.om_size_1();
 
-  sim.static_1_time = 0;
-  sim.static_2_cfl = 0.5;
-  sim.static_5_extent0 = 1.0;
-  sim.static_6_extent1 = 1.0;
-  sim.static_3_dR0 = sim.static_5_extent0 / W;
-  sim.static_4_dR1 = sim.static_6_extent1 / H;
+  sim.time() = 0;
+  sim.cfl() = 0.5;
+  sim.extent0() = 1.0;
+  sim.extent1() = 1.0;
+  sim.dR0() = sim.extent0() / W;
+  sim.dR1() = sim.extent1() / H;
   sim.init();
   char buf[256];
   sprintf(buf, "mkdir -p output%d", antiAlias);
   system(buf);
   int ctr = 0;
   while (ctr <= 100) {
-    double t = sim.static_1_time;
-    cerr << sim.static_1_time << endl;
+    double t = sim.time();
+    cerr << sim.time() << endl;
     if (!isfinite(t)) return -1;
     sim.proceed();
     if (t > 0.01 * ctr) {
